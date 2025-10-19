ecfa575b5c87b8e405716a99508e11e34f74397e
ttnn90395 <ttnn90395@gmail.com>
2025-10-19 09:02:05 +0000

    Cleaning notebook

---
+++ utils.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adamax
+
+class DenseRagged(nn.Module):
+    def __init__(self, in_features=None, out_features=30, activation='relu', use_bias=True):
+        super(DenseRagged, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.use_bias = use_bias
+        self.activation = activation
+
+        self.weight = None
+        self.bias = None
+
+    def forward(self, inputs):
+        outputs = []
+        for x in inputs:
+            if self.weight is None:
+                in_features = x.shape[-1]
+                self.weight = nn.Parameter(torch.randn(in_features, self.out_features) * 0.01)
+                if self.use_bias:
+                    self.bias = nn.Parameter(torch.zeros(self.out_features))
+            y = torch.matmul(x, self.weight)
+            if self.use_bias:
+                y = y + self.bias
+            if self.activation == 'relu':
+                y = F.relu(y)
+            elif self.activation == 'sigmoid':
+                y = torch.sigmoid(y)
+            elif self.activation == 'tanh':
+                y = torch.tanh(y)
+            outputs.append(y)
+        return outputs
+
+
+class PermopRagged(nn.Module):
+    def forward(self, inputs):
+        return torch.stack([torch.sum(x, dim=0) for x in inputs])
+
+
+class RaggedPersistenceModel(nn.Module):
+    def __init__(self, output_dim):
+        super(RaggedPersistenceModel, self).__init__()
+        self.ragged_layers = nn.ModuleList([
+            DenseRagged(out_features=30, activation='relu'),
+            DenseRagged(out_features=20, activation='relu'),
+            DenseRagged(out_features=10, activation='relu')
+        ])
+        self.perm = PermopRagged()
+
+        self.fc = nn.Sequential(
+            nn.Linear(10, 50),
+            nn.ReLU(),
+            nn.Linear(50, 100),
+            nn.ReLU(),
+            nn.Linear(100, 200),
+            nn.ReLU(),
+            nn.Linear(200, output_dim),
+            nn.Sigmoid()
+        )
+
+    def forward(self, inputs):
+        x = inputs
+        for layer in self.ragged_layers:
+            x = layer(x)
+        x = self.perm(x)
+        x = self.fc(x)
+        return x
